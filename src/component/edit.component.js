4b6542f5e8426e2adf39acc9d31d91ec4a55c84a
Tai Tran <transytai12a1@gmail.com>
2019-10-20 19:40:39 +0700

    update edit component

--- src/component/edit.component.js
+++ src/component/edit.component.js
@@ -1,12 +1,74 @@
-
 import React, { Component } from 'react';
+import axios from 'axios'
 
 export default class Edit extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			first_name: '',
+			last_name: ''
+		}
+	}
+
+	handleInputChange = (e) => {
+		const { name, value } = e.target
+		this.setState({...this.state, [name]: value})
+	}
+
+	componentDidMount() {
+		axios.get('http://localhost:3004/users/' + this.props.match.params.id)
+			.then(res => this.setState({
+				first_name: res.data.first_name,
+				last_name: res.data.last_name
+			}))
+			.catch(err => console.log(err))
+	}
+
+	onSubmit = (e) => {
+		const fData = new FormData(e.target)
+
+		axios.put('http://localhost:3004/users/'+this.props.match.params.id, {
+			first_name: fData.get('first_name'),
+			last_name: fData.get('last_name'),
+		})
+			.then(res => console.log(res.data));
+
+		// FIXME: re-render component when using history push
+		// this.props.history.push('/index');
+	}
+
   render() {
     return (
-      <div>
-         <h2 className="text-success"> Welcome to Edit component </h2>
-      </div>
+      <div style={{ marginTop: 10 }}>
+        <h3 align="center">Update User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>First Name:  </label>
+            <input
+              type="text"
+              name="first_name"
+              className="form-control"
+              value={this.state.first_name}
+							onChange={this.handleInputChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Last Name: </label>
+            <input type="text"
+              name="last_name"
+              className="form-control"
+              value={this.state.last_name}
+							onChange={this.handleInputChange}
+            />
+          </div>
+          <div className="form-group">
+            <input type="submit"
+              value="Update"
+              className="btn btn-primary"/>
+            </div>
+          </form>
+        </div>
     )
   }
 }
--- src/component/table-row.js
+++ src/component/table-row.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 export default class TableRow extends Component {
-  onDelete = () => this.props.deleteRow(this.props.obj.id)
+	onDelete = () => this.props.deleteRow(this.props.obj.id)
 
-  render() {
-    return (
-      <tr>
-        <td> {this.props.obj.first_name } </td>
-        <td> {this.props.obj.last_name } </td>
-        <td> <button className="btn btn-primary"> Edit </button> </td>
-        <td> <button className="btn btn-danger" onClick={this.onDelete}> Delete </button></td>
-      </tr>
-    );
-  }
+	render() {
+		return (
+			<tr>
+				<td> {this.props.obj.first_name } </td>
+				<td> {this.props.obj.last_name } </td>
+				<td> <Link to={'/edit/'+ this.props.obj.id} className="btn btn-primary"> Edit </Link> </td>
+				<td> <button className="btn btn-danger" onClick={this.onDelete}> Delete </button></td>
+			</tr>
+		);
+	}
 }
